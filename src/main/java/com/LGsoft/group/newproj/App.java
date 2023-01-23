4b8756f24077bfbbc689373ccbfa2616946b4441
THemanth456 <hemanth.tirumanedi456@gmail.com>
2023-01-23 21:47:40 +0530

    second commit

---
+++ src/main/java/com/LGsoft/group/newproj/App.java
@@ -0,0 +1,13 @@
+package com.LGsoft.group.newproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! this another level" );
+    }
+}
